dfc5d2a71bffbf1d7825b7066c74da875ec09b4d
Abhilash <abhilashmeher1234@gmail.com>
2022-03-27 10:53:40 +0530

    Added program file for UC1

---
+++ LinkedListImp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+
+    public class LinkList
+    {
+        internal Node head;
+
+        public bool Add(int data)
+        {
+            Node n = new Node(data);
+            if (head == null)
+            {
+                head = n;
+                Console.WriteLine("{0} inserted into the link list", n.data);
+                return true;
+            }
+            else
+            {
+                n.next = head;
+                head = n;
+                Console.WriteLine("{0} inserted into the link list", n.data);
+                return true;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello welcome to LinkedList;");
+            LinkList list = new LinkList();
+            list.Add(70);
+            list.Add(30);
+            list.Add(56);
+        }
+    }
+}
